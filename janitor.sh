6562f3b98978b26f3de3b177d7414f8264d24259
andrew <andrewnijmeh1@gmail.com>
2022-10-17 17:20:30 -0400

    gem cleanup + brew tap repair

--- janitor.sh
+++ janitor.sh
@@ -1,9 +1,15 @@
 echo 'now updating & cleaning up...'
 
+# Update & Upgrade then cleanup and repair brew packages
 brew update
 brew upgrade
 brew cleanup
-brew tap --fix
+brew tap --repair
+
+# Cleanup ruby gems
+gem cleanup
+
+# Upgrade rust
 rustup upgrade
 
 echo 'Done.'
